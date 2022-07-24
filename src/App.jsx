224e74e7a96f733afa545d14de5a81a2a3571270
frankuxui <fisdray@gmail.com>
2022-07-24 02:04:58 +0200

    widgets-design

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import './App.css'
 
 function App () {
   return (
-    <div className='App pt-16 pb-16'>
+    <div className='App pt-16 pb-16 flex justify-center flex-col items-center'>
 
       {/** container widgets */}
-      <div className='box-border mx-4 max-w-full sm:columns-2 md:columns-2 lg:columns-4 xl:columns-5 gap-4'>
+      <div className='box-border max-w-7xl mx-4 sm:columns-1 md:columns-2 lg:columns-3 xl:columns-3'>
 
         {/** widget */}
         <div className='break-inside flex items-center justify-between bg-white rounded-xl p-4 mb-4'>
@@ -128,7 +128,7 @@ function App () {
         <div className='break-inside flex items-center space-x-4 bg-white rounded-xl overflow-hidden pr-4 mb-4'>
           <img class='flex-none w-20 h-20 object-cover' src='https://randomuser.me/api/portraits/women/52.jpg' alt='avatar' />
           <div className='flex-auto'>
-            <a href='#' className='no-underline font-bold block text-xl'>Maria</a>
+            <a href='#' className='no-underline font-bold block text-lg'>Maria</a>
           </div>
           <button className='inline-flex justify-center items-center rounded-full p-3 transition-all hover:bg-slate-100'>
             <svg viewBox='0 0 24 24' className='w-6 h-6'>
@@ -167,10 +167,10 @@ function App () {
         {/** widget */}
         <div className='break-inside flex items-center justify-between bg-[#00f5f9] text-black rounded-xl overflow-hidden pr-4 mb-4'>
           <div className='flex flex-1 flex-col p-6'>
-            <div className='font-bold block text-3xl'> New </div>
-            <div className='font-bold block text-3xl'> Releases </div>
+            <div className='font-bold block text-lg'> New </div>
+            <div className='font-bold block text-lg'> Releases </div>
           </div>
-          <img class='flex-none w-32 h-32 object-cover rotate-12 rounded-full -mb-8 -mr-10' src='https://images.pexels.com/photos/1001850/pexels-photo-1001850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='cover' />
+          <img class='flex-none w-28 h-28 object-cover rotate-12 rounded-full -mb-8 -mr-10' src='https://images.pexels.com/photos/1001850/pexels-photo-1001850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='cover' />
         </div>
 
         {/** widget */}
@@ -289,18 +289,18 @@ function App () {
         {/** widget */}
         <div className='break-inside flex items-center justify-between bg-[#ff7300] text-white rounded-xl overflow-hidden pr-4 mb-4'>
           <div className='flex flex-1 flex-col p-6'>
-            <div className='font-bold block text-2xl'> Creadores </div>
-            <div className='font-bold block text-2xl'> de Tendencias </div>
+            <div className='font-bold block text-lg'> Creadores </div>
+            <div className='font-bold block text-lg'> de Tendencias </div>
           </div>
-          <img class='flex-none w-26 h-26 object-cover rotate-12 rounded-lg -mb-10 -mr-6' src='https://randomuser.me/api/portraits/women/58.jpg' alt='avatar' />
+          <img class='flex-none w-24 h-24 object-cover rotate-12 rounded-lg -mb-6 -mr-6' src='https://randomuser.me/api/portraits/women/58.jpg' alt='avatar' />
         </div>
 
         {/** widget */}
         <div className='break-inside flex items-center space-x-4 bg-white rounded-xl overflow-hidden pr-4 mb-4'>
-          <img class='flex-none w-36 h-36 object-cover' src='https://images.pexels.com/photos/343701/pexels-photo-343701.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='avatar' />
-          <div className='flex-auto p-5 flex justify-between items-start'>
+          <img class='flex-none w-28 h-full object-cover' src='https://images.pexels.com/photos/343701/pexels-photo-343701.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='avatar' />
+          <div className='flex-auto flex justify-between items-start'>
             <div className='flex flex-col'>
-              <span className='font-bold text-2xl mb-3'>
+              <span className='font-bold text-lg mb-2'>
                 Pirates of the
                 <br />
                 caribbean
@@ -308,10 +308,10 @@ function App () {
               <small className='text-md text-gray-600'>2022 · Action · Runtime · 2h 5m</small>
             </div>
             <div className='flex justify-center items-center'>
-              <svg viewBox='0 0 24 24' className='w-6 h-6 mr-2'>
+              <svg viewBox='0 0 24 24' className='mr-2' width={22} height={22}>
                 <path fill='#ff00ea' d='M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z' />
               </svg>
-              <span className='font-bold text-lg'>4,95</span>
+              <span className='font-bold text-md'>4.95</span>
             </div>
           </div>
         </div>
@@ -320,9 +320,9 @@ function App () {
         <div className='relative break-inside flex items-center justify-between bg-white rounded-xl p-4 mb-4 overflow-hidden border-2 border-black'>
           <div className='absolute bg-pink-100 top-0 bottom-0 left-0 w-4/6' />
           <div className='flex items-center space-x-4 relative'>
-            <img class='flex-none w-28 h-20 rounded-xl object-cover' src='https://images.pexels.com/photos/33597/guitar-classical-guitar-acoustic-guitar-electric-guitar.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='avatar' />
-            <div className='flex-auto space-y-1'>
-              <h5 className='text-black text-xl font-bold'>Warning</h5>
+            <img class='flex-none w-24 h-18 rounded-xl object-cover' src='https://images.pexels.com/photos/33597/guitar-classical-guitar-acoustic-guitar-electric-guitar.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='avatar' />
+            <div className='flex-auto'>
+              <h5 className='text-black text-lg font-bold'>Warning</h5>
               <h5 className='no-underline block'>Rhemi, Lynn, Lockamy</h5>
             </div>
           </div>
@@ -337,7 +337,7 @@ function App () {
         <div className='break-inside flex flex-col bg-white rounded-xl p-4 mb-4'>
           <div className='flex justify-between items-center mb-4'>
             <div className='flex-auto'>
-              <button type='button' className='py-1 px-4 font-bold block text-lg hover:bg-slate-100 rounded-md'>July 2022</button>
+              <button type='button' className='py-1 px-4 font-bold block text-md hover:bg-slate-100 rounded-md'>July 2022</button>
             </div>
             <div className='justify-center items-center'>
               <button className='items-center justify-center hover:bg-slate-100 rounded-md'>
@@ -390,7 +390,7 @@ function App () {
         <div className='break-inside flex flex-col bg-white rounded-xl p-4 mb-4'>
           <div className='flex justify-between items-center mb-4'>
             <div className='flex-auto'>
-              <button type='button' className='py-1 px-4 font-bold block text-lg hover:bg-slate-100 rounded-md'>Saturday 02</button>
+              <button type='button' className='py-1 px-4 font-bold block text-md hover:bg-slate-100 rounded-md'>Saturday 02</button>
             </div>
             <div className='justify-center items-center'>
               <button className='items-center justify-center hover:bg-slate-100 rounded-md'>
@@ -437,13 +437,13 @@ function App () {
         {/** widget */}
         <div className='break-inside flex bg-white rounded-xl p-4 mb-4'>
           <div className='flex items-start space-x-4'>
-            <svg width='64' height='64' viewBox='0 0 1024 1024' fill='#232f3e'>
+            <svg width='60' height='60' viewBox='0 0 1024 1024' fill='#232f3e'>
               <circle cx='512' cy='512' r='512' />
               <path fill='white' d='M400.3 479.1c0 6.3.7 11.4 1.9 15.1 1.4 3.8 3.1 7.9 5.5 12.3.9 1.3 1.2 2.7 1.2 3.9 0 1.7-1 3.4-3.2 5.1l-10.7 7.1c-1.3.9-2.8 1.5-4.4 1.5-1.7 0-3.4-.9-5.1-2.4-2.3-2.5-4.3-5.1-6.1-8-1.9-3.3-3.7-6.6-5.3-10-13.3 15.7-30 23.5-50.1 23.5-14.3 0-25.7-4.1-34-12.2-8.3-8.2-12.6-19.1-12.6-32.7 0-14.5 5.1-26.2 15.5-35.1 10.4-8.9 24.2-13.3 41.7-13.3 5.8 0 11.8.5 18 1.4 6.3.9 12.8 2.2 19.6 3.8v-12.4c0-12.9-2.7-22-8-27.2-5.4-5.3-14.6-7.8-27.7-7.8-6 0-12.1.7-18.4 2.2s-12.4 3.4-18.4 5.8c-1.9.9-3.9 1.6-6 2.2-.9.3-1.8.4-2.7.5-2.4 0-3.6-1.7-3.6-5.3v-8.3c0-2.7.3-4.8 1.2-6 1.3-1.5 2.9-2.8 4.8-3.6 6-3.1 13.1-5.6 21.4-7.7 8.7-2.2 17.6-3.3 26.6-3.2 20.3 0 35.1 4.6 44.6 13.8 9.4 9.2 14.1 23.1 14.1 41.9v55.2l.2-.1zM331.2 505c5.6 0 11.4-1 17.5-3.1 6.1-2 11.6-5.8 16.2-10.9 2.7-3.2 4.8-6.8 5.8-10.9 1-4.1 1.7-9 1.7-14.8v-7.1c-5.2-1.3-10.4-2.2-15.7-2.9-5.3-.7-10.7-1-16-1-11.4 0-19.8 2.2-25.4 6.8-5.6 4.6-8.3 11.1-8.3 19.6 0 8 2 14 6.3 18 4.1 4.3 10 6.3 17.9 6.3zm136.7 18.4c-3.1 0-5.1-.5-6.5-1.7-1.4-1-2.6-3.4-3.6-6.6l-40-131.6c-.8-2.2-1.3-4.5-1.5-6.8 0-2.7 1.4-4.3 4.1-4.3h16.7c3.2 0 5.4.5 6.6 1.7 1.4 1 2.4 3.4 3.4 6.7l28.6 112.7 26.6-112.7c.9-3.4 1.9-5.6 3.2-6.7 2-1.3 4.4-1.9 6.8-1.7H526c3.2 0 5.5.5 6.8 1.7 1.3 1 2.6 3.4 3.2 6.7l26.9 114.1 29.5-114.1c1-3.4 2.2-5.6 3.4-6.7 2-1.3 4.3-1.9 6.6-1.7h15.9c2.7 0 4.3 1.4 4.3 4.3 0 .9-.2 1.7-.4 2.7-.3 1.5-.7 2.9-1.2 4.3l-41 131.6c-1 3.4-2.2 5.6-3.6 6.6-1.9 1.2-4.2 1.8-6.5 1.7h-14.7c-3.2 0-5.4-.5-6.8-1.7-1.3-1.2-2.5-3.4-3.2-6.8l-26.4-109.8L492.6 515c-.9 3.4-1.9 5.6-3.2 6.8-1.4 1.2-3.8 1.7-6.8 1.7l-14.7-.1zm218.8 4.6c-8.9 0-17.7-1-26.2-3.1-8.5-2-15.1-4.3-19.6-6.8-2.7-1.5-4.6-3.2-5.3-4.8-.7-1.5-1-3.1-1-4.8v-8.7c0-3.6 1.4-5.3 3.9-5.3 1 0 2 .2 3.1.5 1 .3 2.6 1 4.3 1.7 5.8 2.6 12.1 4.6 18.7 6 6.8 1.4 13.4 2 20.3 2 10.7 0 19.1-1.9 24.9-5.6 5.6-3.4 9-9.6 8.9-16.2.1-4.4-1.5-8.7-4.6-11.9-3.1-3.2-8.9-6.1-17.2-8.9l-24.7-7.7c-12.4-3.9-21.6-9.7-27.2-17.3-5.5-7.1-8.5-15.8-8.5-24.7 0-7.1 1.6-13.4 4.6-18.9 3.1-5.4 7.1-10.2 12.3-14 5.1-3.9 10.9-6.8 17.7-8.9 6.8-2 14-2.9 21.5-2.9 3.7 0 7.7.2 11.4.7 3.9.5 7.5 1.2 11.1 1.9 3.4.9 6.7 1.7 9.7 2.7 3.1 1 5.5 2 7.2 3.1 2 1 3.8 2.5 5.1 4.3 1.1 1.7 1.6 3.6 1.5 5.6v8c0 3.6-1.4 5.5-3.9 5.5-2.3-.3-4.5-1-6.5-2-10.3-4.5-21.4-6.8-32.7-6.6-9.7 0-17.4 1.5-22.7 4.8-5.3 3.2-8 8.2-8 15.1 0 4.8 1.7 8.9 5.1 12.1 3.4 3.2 9.7 6.5 18.7 9.4l24.2 7.6c12.2 3.9 21.1 9.4 26.4 16.4 5.3 7 7.8 15 7.8 23.8 0 7.3-1.5 14-4.4 19.8-3.1 5.8-7.2 10.9-12.4 15-5.3 4.3-11.6 7.3-18.9 9.5-7.9 2.4-15.9 3.6-24.6 3.6zm32.2 82.7c-56 41.4-137.4 63.3-207.4 63.3-98.1 0-186.5-36.3-253.2-96.6-5.3-4.8-.5-11.2 5.8-7.5 72.2 41.9 161.3 67.3 253.4 67.3 62.2 0 130.4-12.9 193.3-39.5 9.3-4.2 17.3 6.2 8.1 13zm23.3-26.5c-7.2-9.2-47.4-4.4-65.6-2.2-5.4.7-6.3-4.1-1.3-7.7 32-22.5 84.6-16 90.8-8.5 6.1 7.7-1.7 60.3-31.7 85.5-4.6 3.9-9 1.9-7-3.2 6.8-16.9 22-54.9 14.8-63.9z' />
             </svg>
 
             <div className='flex-auto'>
-              <h5 className='no-underline font-bold text-2xl mb-2'>Amazon</h5>
+              <h5 className='no-underline font-bold text-lg mb-2'>Amazon</h5>
               <div className='flex items-center'>
                 <span className='mr-2 px-2 py-1 bg-slate-200 rounded-md'>
                   <svg width='16' height='16' viewBox='0 0 24 24'>
@@ -458,8 +458,8 @@ function App () {
                 </span>
                 <span className='text-slate-400 text-sm'>19:23</span>
               </div>
-              <div className='mt-2'>
-                <a href='#' className='text-blue-500 font-bold inline-flex items-center'>
+              <div className='mt-1'>
+                <a href='#' className='text-blue-500 text-sm font-bold inline-flex items-center'>
                   If was not you?, click here
                   <svg width='20' height='20' viewBox='0 0 24 24' className='ml-2'>
                     <path fill='currentColor' d='M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z' />
