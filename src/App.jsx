0be84d8987aebfe395a48222ecc992f246ee7566
frankuxui <fisdray@gmail.com>
2022-07-26 21:48:00 +0200

    widget-26

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,27 @@ function App () {
         </div>
 
         {/** widget */}
+        <div className='break-inside flex items-center space-x-4 bg-white rounded-xl overflow-hidden pr-4 mb-4'>
+          <img class='flex-none w-24 h-24 object-cover' src='https://randomuser.me/api/portraits/men/42.jpg' alt='avatar' />
+          <div className='flex rounded-3xl bg-slate-200 p-2 w-full justify-between'>
+            <div className='flex items-center justify-center bg-white p-2 rounded-full'>
+              <button>
+                <svg width='20' height='20' viewBox='0 0 24 24'>
+                  <path fill='currentColor' d='M12,17C10.89,17 10,16.1 10,15C10,13.89 10.89,13 12,13A2,2 0 0,1 14,15A2,2 0 0,1 12,17M18,20V10H6V20H18M18,8A2,2 0 0,1 20,10V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V10C4,8.89 4.89,8 6,8H7V6A5,5 0 0,1 12,1A5,5 0 0,1 17,6V8H18M12,3A3,3 0 0,0 9,6V8H15V6A3,3 0 0,0 12,3Z' />
+                </svg>
+              </button>
+            </div>
+            <div className='flex items-center justify-center bg-white p-2 rounded-full'>
+              <button>
+                <svg width='20' height='20' viewBox='0 0 24 24'>
+                  <path fill='currentColor' d='M10 13C11.1 13 12 13.89 12 15C12 16.11 11.11 17 10 17S8 16.11 8 15 8.9 13 10 13M18 1C15.24 1 13 3.24 13 6V8H4C2.9 8 2 8.9 2 10V20C2 21.1 2.9 22 4 22H16C17.1 22 18 21.1 18 20V10C18 8.9 17.1 8 16 8H15V6C15 4.34 16.34 3 18 3S21 4.34 21 6V8H23V6C23 3.24 20.76 1 18 1M16 10V20H4V10H16Z' />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/** widget */}
         <div className='flex flex-col break-inside bg-white rounded-xl p-4 mb-4'>
           <div className='flex items-center justify-between mb-3'>
             <div className='flex flex-col items-start justify-center'>
