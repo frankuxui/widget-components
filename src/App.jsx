4bc2b0bce5e92d5e565746a1422e1ebd91d268e6
frankuxui <fisdray@gmail.com>
2022-07-26 22:02:43 +0200

    widget-27

--- src/App.jsx
+++ src/App.jsx
@@ -26,15 +26,6 @@ function App () {
 
         {/** widget */}
         <div className='break-inside flex items-center space-x-4 bg-white rounded-xl overflow-hidden pr-4 mb-4'>
-          <img class='flex-none w-24 h-24 object-cover' src='https://randomuser.me/api/portraits/women/45.jpg' alt='avatar' />
-          <div className='flex-auto'>
-            <a href='#' className='no-underline font-bold block'>Julia Robert</a>
-            <span className='text-slate-600'>Frontend Development</span>
-          </div>
-        </div>
-
-        {/** widget */}
-        <div className='break-inside flex items-center space-x-4 bg-white rounded-xl overflow-hidden pr-4 mb-4'>
           <img class='flex-none w-24 h-24 object-cover' src='https://randomuser.me/api/portraits/men/42.jpg' alt='avatar' />
           <div className='flex rounded-3xl bg-slate-200 p-2 w-full justify-between'>
             <div className='flex items-center justify-center bg-white p-2 rounded-full'>
@@ -50,6 +41,30 @@ function App () {
                   <path fill='currentColor' d='M10 13C11.1 13 12 13.89 12 15C12 16.11 11.11 17 10 17S8 16.11 8 15 8.9 13 10 13M18 1C15.24 1 13 3.24 13 6V8H4C2.9 8 2 8.9 2 10V20C2 21.1 2.9 22 4 22H16C17.1 22 18 21.1 18 20V10C18 8.9 17.1 8 16 8H15V6C15 4.34 16.34 3 18 3S21 4.34 21 6V8H23V6C23 3.24 20.76 1 18 1M16 10V20H4V10H16Z' />
                 </svg>
               </button>
+            </div>
+          </div>
+        </div>
+
+        {/** widget */}
+        <div className='break-inside flex items-center space-x-4 bg-white rounded-xl overflow-hidden pr-4 mb-4'>
+          <img class='flex-none w-24 h-24 object-cover' src='https://randomuser.me/api/portraits/women/45.jpg' alt='avatar' />
+          <div className='flex-auto'>
+            <a href='#' className='no-underline font-bold block'>Julia Robert</a>
+            <span className='text-slate-600'>Frontend Development</span>
+          </div>
+        </div>
+
+        {/** widget */}
+        <div className='break-inside flex bg-white rounded-xl p-4 mb-4'>
+          <div className='flex flex-row justify-between items-start w-full'>
+            <div className='flex flex-col justify-center items-center'>
+              <span className='font-bold block'>Jun</span>
+              <span className='text-lg font-bold text-pink-500'>23</span>
+            </div>
+            <div className='border h-full bg-slate-200' />
+            <div className='flex flex-col justify-center items-start'>
+              <span className='font-bold block mb-1'>Wednesday</span>
+              <span className='text-sm text-slate-400'>08:00 PM - 18:30 PM</span>
             </div>
           </div>
         </div>
