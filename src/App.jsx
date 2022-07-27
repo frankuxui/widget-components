e0ef56387c2e2d17ee1f2660d515f16f2880bc16
frankuxui <fisdray@gmail.com>
2022-07-27 22:25:51 +0200

    widget-42

--- src/App.jsx
+++ src/App.jsx
@@ -475,6 +475,17 @@ function App () {
         </div>
 
         {/** widget */}
+        <div className='break-inside relative overflow-hidden flex flex-col justify-between bg-white space-y-2 rounded-xl p-4 mb-4'>
+          <div className='flex flex-row items-center space-x-2'>
+            <svg width='38' height='38' viewBox='0 0 24 24'>
+              <path fill='#1ED760' d='M17.9,10.9C14.7,9 9.35,8.8 6.3,9.75C5.8,9.9 5.3,9.6 5.15,9.15C5,8.65 5.3,8.15 5.75,8C9.3,6.95 15.15,7.15 18.85,9.35C19.3,9.6 19.45,10.2 19.2,10.65C18.95,11 18.35,11.15 17.9,10.9M17.8,13.7C17.55,14.05 17.1,14.2 16.75,13.95C14.05,12.3 9.95,11.8 6.8,12.8C6.4,12.9 5.95,12.7 5.85,12.3C5.75,11.9 5.95,11.45 6.35,11.35C10,10.25 14.5,10.8 17.6,12.7C17.9,12.85 18.05,13.35 17.8,13.7M16.6,16.45C16.4,16.75 16.05,16.85 15.75,16.65C13.4,15.2 10.45,14.9 6.95,15.7C6.6,15.8 6.3,15.55 6.2,15.25C6.1,14.9 6.35,14.6 6.65,14.5C10.45,13.65 13.75,14 16.35,15.6C16.7,15.75 16.75,16.15 16.6,16.45M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z' />
+            </svg>
+            <span className='font-bold block'>Enjoy songs</span>
+          </div>
+          <div className='absolute -right-4 -top-4 rounded-full w-24 h-24 bg-green-100' />
+        </div>
+
+        {/** widget */}
         <div className='break-inside flex items-center justify-between bg-white rounded-xl p-4 mb-4'>
           <div className='flex items-center space-x-4'>
             <img class='flex-none w-12 h-12 rounded-full object-cover' src='https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt='avatar' />
