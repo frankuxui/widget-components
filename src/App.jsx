6e549311fbdf05ebfe5085cd487e78f4d9703690
frankuxui <fisdray@gmail.com>
2022-07-28 20:22:01 +0200

    widget-83

--- src/App.jsx
+++ src/App.jsx
@@ -1246,6 +1246,25 @@ function App () {
         </div>
 
         {/* widget */}
+        <div className='break-inside flex justify-between bg-white items-center rounded-xl p-4 mb-4'>
+          <div className='flex items-center space-x-3 bg-slate-100 rounded-full px-2 py-2'>
+            <div className='flex flex-none justify-center items-center w-10 h-10 bg-[#FF9900] rounded-full font-medium text-white text-xl ring-2 ring-[#FF9900] border-2 border-white'>
+              <svg width="26" height="26" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17.06 11.57C17.65 10.88 18 10 18 9C18 7.14 16.73 5.57 15 5.13V3H13V5H11V3H9V5H6V7H8V17H6V19H9V21H11V19H13V21H15V19C17.21 19 19 17.21 19 15C19 13.55 18.22 12.27 17.06 11.57M10 7H14C15.1 7 16 7.9 16 9S15.1 11 14 11H10V7M15 17H10V13H15C16.1 13 17 13.9 17 15S16.1 17 15 17Z" fill='currentColor'/>
+              </svg>
+            </div>
+            <div className='flex items-center'>
+              <span className='font-medium'>USD</span>
+              <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' className='text-slate-800' viewBox='0 0 24 24'>
+                <path d='M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z' />
+              </svg>
+            </div>
+          </div>
+          <span className='font-bold text-lg'>Balance</span>
+          <span className='text-red-500 font-bold'>$3427.00</span>
+        </div>
+
+        {/* widget */}
         <div className='break-inside flex flex-row justify-between bg-white items-center rounded-xl p-4 mb-4'>
           working...
         </div>
