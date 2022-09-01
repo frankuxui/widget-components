2b40664ad6ad9d8dd7ac4dc96dbc3e2eff42fbeb
frankuxui <fisdray@gmail.com>
2022-09-01 20:06:30 +0200

    widget-97

--- src/App.jsx
+++ src/App.jsx
@@ -604,6 +604,66 @@ function App () {
         </div>
 
         {/** widget */}
+        <div className='break-inside flex items-center flex-row justify-between rounded-xl p-4 mb-4 text-sm bg-[#101C0E] text-white'>
+          <div className='flex flex-col items-start space-y-1'>
+            <span className='uppercase text-xs font-medium text-[#88aa82] dark:text-slate-400'>CURRENT VALUE</span>
+            <div className='flex items-center justify-center space-x-2'>
+              <h5 className='font-bold'>€657.000</h5>
+              <span className='px-2 py-[2px] text-xs rounded-md bg-[#31492e] text-white'>-082%</span>
+            </div>
+            <p className='text-xs'>Update now - 3:23AM</p>
+          </div>
+          <div className='flex flex-col space-y-2'>
+            <div className='flex flex-row items-center'>
+              <svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+                <path d='M17 17L7.8 7.7M7 17V7h10' />
+              </svg>
+              <span className='font-bold block'>Upward</span>
+            </div>
+            <div>
+              <svg width='139' height='37' viewBox='0 0 139 37' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <g filter='url(#filter0_d_417_13)'>
+                  <path d='M4 1.03702C20.6434 0.736031 21.9399 12.2896 33.9648 17.0061C39.1853 19.0537 46.7323 20.0614 47.5 22.3291C48.9607 26.6439 55.5137 13.1371 58.691 15.7991C61.3177 17.9998 66.6169 10.5 70 10.5C71.2905 10.5 69.5663 21.5533 70.8795 22.3291C75.5857 25.1094 99.9248 -1.07309 100.181 7.65985C100.267 10.5859 103.228 14.7527 106.729 14.9326C111.327 15.1688 115.088 13.9676 117.887 18.0274C120.415 21.6933 124.071 23.8765 128.941 23.8765C132.089 23.8765 131.858 20.6288 135 24.0312' stroke='url(#paint0_linear_417_13)' strokeWidth='2' strokeLinecap='round' />
+                </g>
+                <g filter='url(#filter1_d_417_13)'>
+                  <path d='M19 1C30.5 8.49999 21.9399 16.2896 33.9648 21.0061C39.1853 23.0537 48.7907 10.8931 49.5584 13.1608C51.2366 18.118 67.8515 24.5403 70.8795 26.3291C75.5857 29.1094 97.244 8.76707 97.5 17.5C97.5858 20.426 103.228 18.7528 106.729 18.9326C111.327 19.1688 116.2 4.44024 119 8.49999C121.528 12.1659 124.071 27.8765 128.941 27.8765C132.089 27.8765 131.858 24.6288 135 28.0313' stroke='url(#paint1_linear_417_13)' stroke-width='2' stroke-linecap='round' />
+                </g>
+                <defs>
+                  <filter id='filter0_d_417_13' x='0' y='0.03125' width='139' height='32' filterUnits='userSpaceOnUse' colorInterpolationFilters='sRGB'>
+                    <feFlood floodOpacity='0' result='BackgroundImageFix' />
+                    <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha' />
+                    <feOffset dy='4' />
+                    <feGaussianBlur stdDeviation='1.5' />
+                    <feComposite in2='hardAlpha' operator='out' />
+                    <feColorMatrix type='matrix' values='0 0 0 0 0.00392157 0 0 0 0 0.945098 0 0 0 0 0.52549 0 0 0 0.12 0' />
+                    <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_417_13' />
+                    <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_417_13' result='shape' />
+                  </filter>
+                  <filter id='filter1_d_417_13' x='14.9999' y='-0.000114441' width='124' height='36.0314' filterUnits='userSpaceOnUse' colorInterpolationFilters='sRGB'>
+                    <feFlood floodOpacity='0' result='BackgroundImageFix' />
+                    <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha' />
+                    <feOffset dy='4' />
+                    <feGaussianBlur stdDeviation='1.5' />
+                    <feComposite in2='hardAlpha' operator='out' />
+                    <feColorMatrix type='matrix' values='0 0 0 0 0.00392157 0 0 0 0 0.945098 0 0 0 0 0.52549 0 0 0 0.12 0' />
+                    <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_417_13' />
+                    <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_417_13' result='shape' />
+                  </filter>
+                  <linearGradient id='paint0_linear_417_13' x1='4' y1='1.03125' x2='138.386' y2='19.4412' gradientUnits='userSpaceOnUse'>
+                    <stop stopColor='#00FFA3' />
+                    <stop offset='1' stopColor='#00EA5E' />
+                  </linearGradient>
+                  <linearGradient id='paint1_linear_417_13' x1='3.99999' y1='5.0313' x2='138.386' y2='23.4413' gradientUnits='userSpaceOnUse'>
+                    <stop stopColor='#05FF00' />
+                    <stop offset='1' stopColor='#ADEA00' />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        {/** widget */}
         <button type='button' className='break-inside text-center bg-[#1E49E2] rounded-xl p-4 mb-4 w-full'>
           <span className='uppercase font-medium text-white'>Send files now</span>
         </button>
@@ -2384,6 +2444,27 @@ function App () {
             <img src='https://images.pexels.com/photos/2479312/pexels-photo-2479312.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' className='object-cover' />
           </div>
         </a>
+
+        {/** widget */}
+        <div className='break-inside relative overflow-hidden flex flex-col justify-between space-y-2 text-sm rounded-xl p-4 mb-4 bg-[#5E17F4] text-white'>
+          <span className='uppercase text-xs text-[#D2BDFF]'>upgrade team</span>
+          <div className='flex flex-row items-center space-x-3'>
+            <svg width='58' height='56' viewBox='0 0 52 50' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <path d='M32.6458 38.4379C33.9918 37.1198 33.2655 34.0922 31.0668 30.5948C31.8658 30.4707 32.6129 30.281 33.178 29.9905C35.2112 28.9466 36.584 27.044 37.6232 25.0759C38.7403 22.9647 39.49 20.644 40.9477 18.7215C41.1939 18.3966 41.44 18.1052 41.6853 17.831C44.8304 18.206 47.3412 18.8784 47.3412 18.8784L48.3006 16.4534C47.0896 16.0212 45.848 15.6791 44.586 15.4302C45.3591 14.9931 45.8635 14.8569 45.8635 14.8569L44.9543 12.4121C43.4966 13.025 42.3136 13.9293 41.323 15.0121C37.6206 14.806 33.8921 15.5397 30.9506 17.8086C28.7389 19.5155 27.2447 21.8819 25.839 24.2491C24.5935 23.0333 23.2671 21.9023 21.8688 20.8638C22.134 20.4302 22.4182 20.0405 22.7242 19.7397C24.5728 17.9293 27.0116 16.7716 28.6115 14.7C31.9742 10.35 29.5146 3.53103 26.7481 0C26.2524 0.475 25.4325 1.16724 24.8155 1.71379C27.7561 4.70948 29.8127 9.95431 27.5082 13.8733C26.2203 16.0638 23.8404 17.4379 22.1764 19.3198C21.8887 19.6466 21.6313 20.0603 21.3982 20.5172C17.0466 17.4129 13.053 16.1638 11.4704 17.7138C11.3133 17.8737 11.1838 18.0584 11.0874 18.2603L11.0813 18.2543L11.0388 18.3776C10.9799 18.5112 10.9261 18.65 10.8897 18.8017L0 50L31.774 38.95L31.7653 38.9414C32.1068 38.8319 32.4075 38.6707 32.6458 38.4379ZM6.32065 45.9759L3.66863 44.7465L5.45831 39.6172L13.6666 43.4207L6.32065 45.9759ZM21.0116 40.8664L7.24972 34.4879L9.0394 29.3595L19.3233 34.494C13.1847 30.5198 10.8291 24.2293 10.8291 24.2293L11.441 22.4767C12.5286 25.2138 14.9215 28.6224 18.2097 31.8397C21.5256 35.0862 25.0399 37.4379 27.8488 38.4888L21.0116 40.8664ZM26.2975 24.7112C27.7344 22.6621 29.2156 20.594 31.2748 19.1224C33.2352 17.7207 36.4176 17.4647 39.4345 17.6328C38.4153 19.4034 37.6622 21.3681 36.9861 23.2552C36.1689 25.5397 35.0734 27.9086 32.9847 29.3095C32.4214 29.6871 31.6318 29.9629 30.7886 30.1672C29.6298 28.4009 28.1097 26.5336 26.2975 24.7112Z' fill='white' />
+              <path d='M18.2287 16.3793C19.0971 16.3793 16.4937 13.7931 16.9287 11.525C18.5962 11.3974 22.4078 12.1448 20.1892 9.11379C22.699 9.55345 23.9991 7.68966 21.6296 5.92328C22.4182 5.97845 23.6437 4.49914 22.764 4.31207C19.9456 3.7181 18.8423 5.23448 20.6312 7.42155C18.7505 7.07328 17.2173 7.9431 18.63 9.89655C13.1994 9.22328 16.2891 16.3793 18.2287 16.3793ZM36.8726 14.081C37.6864 13.7155 36.3058 11.3009 35.8569 10.6836C39.2915 10.3181 39.1615 9.3 37.0078 7.11897C42.8631 7.31466 37.1889 4.00431 37.9846 2.69397C38.6736 1.55776 40.7874 2.74914 40.5915 2.11638C39.9311 0 33.6668 1.43103 37.631 5.38276C34.1712 5.45 33.8393 6.575 36.4176 8.9069C31.9265 8.95603 35.5908 14.6552 36.8726 14.081ZM51.7378 22.6078C50.3667 22.9897 50.1553 22.8466 50.3381 24.2043C47.1713 22.7543 43.8207 20.7379 45.854 26.0802C42.2573 23.95 42.4367 25.8155 41.7641 28.8853C40.8888 28.2069 39.6451 26.419 39.6451 26.419L38.3278 27.5319C38.3278 27.5319 40.7414 30.9181 41.9331 30.7259C42.9809 30.5578 43.5512 28.5879 43.6093 26.8517C46.946 28.2526 48.5432 28.4397 47.017 24.3431C49.6846 25.8336 52.9555 27.1483 51.7378 22.6078ZM3.50916 7.27328L5.96011 9.71207L3.50916 12.15L1.05734 9.71207L3.50916 7.27328ZM24.1005 26.5181L21.6478 28.956L19.1959 26.5164L21.6486 24.0776L24.1005 26.5181ZM13.1908 3.44828L15.6417 5.88621L13.1899 8.32586L10.7389 5.88621L13.1908 3.44828ZM39.8765 37.4862L37.4238 35.0474L39.8748 32.6078L42.3275 35.0466L39.8765 37.4862ZM34.4113 45.85L31.9603 43.4121L34.4113 40.9733L36.8631 43.4121L34.4113 45.85ZM45.1649 47.7759L42.7123 45.3371L45.1623 42.8974L47.615 45.3362L45.1649 47.7759ZM47.6159 36.669L45.1649 34.2302L47.6159 31.7922L50.0668 34.2302L47.6159 36.669ZM43.5243 6.03448L45.9753 8.47241L43.5235 10.9112L41.0725 8.47241L43.5243 6.03448Z' fill='white' />
+            </svg>
+            <span className='text-base font-medium'>Congratulations, your team has improved!</span>
+          </div>
+          <div className='flex justify-between items-center'>
+            <span>Modify your data</span>
+            <button className='flex items-center justify-center text-xs font-medium rounded-full px-4 py-2 space-x-1 bg-white text-black'>
+              <span>Next step</span>
+              <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='#000000' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+                <path d='M5 12h13M12 5l7 7-7 7' />
+              </svg>
+            </button>
+          </div>
+        </div>
 
       </div>
     </div>
